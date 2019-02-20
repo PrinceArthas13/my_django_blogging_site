eee7d77e4020302d09923c85f1db3ed78d61042f
Dennis <dferenchik@gmail.com>
2019-02-20 16:01:18 -0500

    Add feature to make drafts and publish them.

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     # 127.0.0.1:8000(local) this is where this takes you
     # my.djangosite.com(online)
@@ -14,4 +15,10 @@ urlpatterns = [
 
     #127.0.0.1:8000/post/2/edit(local)
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
+
+    #127.0.0.1:8000/drafts(local)
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+
+    #127.0.0.1:8000/publish/(local)
+    path('post/<int:pk>/publish/', views.post_publish, name="post_publish"),
 ]
