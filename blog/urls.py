878b56ca1de5e9ee189cb24a75f22d6bb78cc161
Dennis <dferenchik@gmail.com>
2019-02-26 14:09:23 -0500

    Add feature to approve comments on blog posts.

--- blog/urls.py
+++ blog/urls.py
@@ -27,5 +27,8 @@ urlpatterns = [
 
     #127.0.0.1:8000/comment/2/remove(local)
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
+
+    #127.0.0.1:8000/comment/2/approve(local)
+    path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
 ]
 
